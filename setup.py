35e648452a9d3c3008492b5a07ea60be877a584d
ahmad-kemsan <ahmad-kemsan@users.noreply.github.com>
2026-01-02 11:48:25 +0000

    chore(package version): updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cryptlex.lexfloatclient",
-    version="4.12.2",
+    version="4.13.0",
     author="Cryptlex LLP",
     author_email="support@cryptlex.com",
     description="LexFloatClient API wrapper for Python",
